a9628cbe9a373d59d0ba0f76d82c5120fc717e31
Alexandr <Shurlik003@gmail.com>
2022-11-28 19:59:14 +0200

    Lab2 start

---
+++ Lab2MainModule/src/main/java/khai/edu/com/Lab2Main.java
@@ -0,0 +1,25 @@
+package khai.edu.com;
+
+import java.util.Scanner;
+public class Lab2Main{
+    public static void main(String[] args) {
+        int decision = 0;
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println(
+                    "Please press any number if you want to start the program or press 0 if you want to leave");
+            while (!sc.hasNextInt()) {
+                sc.next();
+                System.out.println("Wrong input, try again");
+            }
+            decision = sc.nextInt();
+            switch (decision) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+            }
+            break;
+        }
+    }
+}
